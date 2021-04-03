62aab75fb54f98905e8ef0333d601b8b7514d96d
vinoddk <getbenifit35@gmail.com>
2021-04-03 19:46:48 -0400

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,4 +1,13 @@
-resource "aws_instance" "web1" {
-   ami           = "${lookup(var.ami_id, var.region)}"
-   instance_type = "t2.micro"
- }
+# resource "aws_instance" "web1" {
+#    ami           = "${lookup(var.ami_id, var.region)}"
+#    instance_type = "t2.micro"
+#  }
+resource "aws_s3_bucket" "bucket_test" {
+  bucket = "my-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "my-test-bucket"
+    Environment = "Dev"
+  }
+}
